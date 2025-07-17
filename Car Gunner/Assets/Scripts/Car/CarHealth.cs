e1e831a275442b41afeb99e497d3d525bff3a522
OlegSalamashenko <olegpups901@gmail.com>
2025-07-17 23:00:22 +0300

    Added HealBar

--- Car Gunner/Assets/Scripts/Car/CarHealth.cs
+++ Car Gunner/Assets/Scripts/Car/CarHealth.cs
@@ -1,28 +1,26 @@
 using UnityEngine;
-using UnityEngine.UI;
-using System;
-
-public class CarHealth : MonoBehaviour
-{
-    [SerializeField] private int maxHealth = 15;
-
-    private int _currentHealth;
-
-    public event Action OnCarDestroyed;
-
-    private void Start()
-    {
-        _currentHealth = maxHealth;
-    }
-
-    public void TakeDamage(int amount)
-    {
-        Debug.Log("_currentHealth  "+_currentHealth);
-        _currentHealth -= amount;
-        if (_currentHealth <= 0)
-        {
-            _currentHealth = 0;
-            OnCarDestroyed?.Invoke();
-        }
-    }
-}
+ using UnityEngine.UI;
+ using System;
+ 
+ public class CarHealth : MonoBehaviour
+ {
+     [SerializeField] private int maxHealth = 15;
+     private int _currentHealth;
+     public event Action<int,int> OnHealthChanged;
+     public event Action OnCarDestroyed;
+ 
+     private void Start()
+     {
+         _currentHealth = maxHealth;
+         OnHealthChanged?.Invoke(_currentHealth, maxHealth);
+     }
+ 
+     public void TakeDamage(int amount)
+     {
+         _currentHealth = Mathf.Clamp(_currentHealth - amount, 0, maxHealth);
+         OnHealthChanged?.Invoke(_currentHealth, maxHealth);
+ 
+         if (_currentHealth == 0)
+             OnCarDestroyed?.Invoke();
+     }
+ }
